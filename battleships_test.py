b7e76402f2bc086009619e331d1eb19f96bdc762
MarsRaptor <waltongregory.hobby@gmail.com>
2021-10-21 14:26:50 +0200

    Initial workflow setup and test

---
+++ battleships_test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestWorkflow(unittest.TestCase):
+    def test_1_is_this_on(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
